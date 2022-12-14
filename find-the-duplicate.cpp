eceb0e291f8b7367826257029077ffe35991fb34
Boi <119715263+cpp-johnny@users.noreply.github.com>
2022-12-14 11:04:16 +0800

    Update find-the-duplicate.cpp

--- find-the-duplicate.cpp
+++ find-the-duplicate.cpp
@@ -1,6 +1,7 @@
 /*
 Solve the problem [C++]
-Given an array of integers arr that contains n+1 elements between 1 and n inclusive, create a function that returns the duplicate element (the element that appears more than once). Assume that:
+Given an array of integers arr that contains n+1 elements between 1 and n inclusive, 
+create a function that returns the duplicate element (the element that appears more than once). Assume that:
 - There is only one duplicate number
 - The duplicate number can be repeated more than once
 
